ea90728cfa6de57f9e3980829d2f155a54c988de
Kaneko Yoshiki <yoshiki.kaneko@retailstudio.jp>
2022-12-05 15:13:26 +0900

    apollo serverの初期設定

---
+++ packages/bff/src/index.ts
@@ -0,0 +1,29 @@
+import { ApolloServer } from "apollo-server-express";
+import express from "express";
+import { ApolloServerPluginLandingPageLocalDefault } from "apollo-server-core";
+import { schema } from "./schema";
+
+const port = process.env.PORT || 1111;
+
+const app = express();
+
+app.use(express.json());
+
+const server = new ApolloServer({
+  schema,
+  plugins: [ApolloServerPluginLandingPageLocalDefault()],
+});
+
+const main = async () => {
+  await server.start();
+  server.applyMiddleware({
+    app,
+    path: "/graphql",
+  });
+
+  app.listen({ port, host: "0.0.0.0" }, () => {
+    console.log(`🙆‍♂️ http://localhost:${port}/graphql`);
+  });
+};
+
+main();
