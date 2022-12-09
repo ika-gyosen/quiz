bb99d26b6c9b7ae78c14d97121b70856a051a8d9
Kaneko Yoshiki <yoshiki.kaneko@retailstudio.jp>
2022-12-09 13:55:52 +0900

    apollo providerを作成した

--- packages/searchApp/src/App.tsx
+++ packages/searchApp/src/App.tsx
@@ -1,5 +1,8 @@
+import { ApolloProvider } from '~/providers/Apollo';
+// import { Test } from '~/Test';
+
 const App = () => {
-  return <div>quiz</div>;
+  return <ApolloProvider>quiz</ApolloProvider>;
 };
 
 export default App;
