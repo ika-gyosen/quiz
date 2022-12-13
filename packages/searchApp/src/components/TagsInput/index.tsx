55f64f962b2b3fddbae9ae9e1b903f41807f9fae
ika-gyosen(sakai) <s.sakai4223@gmail.com>
2022-12-13 16:26:27 +0900

    feat: create multiple tags input

---
+++ packages/searchApp/src/components/TagsInput/index.tsx
@@ -0,0 +1,31 @@
+import * as React from 'react';
+import Autocomplete from '@mui/material/Autocomplete';
+import TextField from '@mui/material/TextField';
+
+export type Option<T> = {
+  value: T;
+  label: string;
+};
+type Props<T> = {
+  values: Option<T>[];
+  options: Option<T>[];
+  onChange: (values: Option<T>[]) => void;
+};
+
+export const TagsInput = <T extends string | number>({
+  onChange,
+  values,
+  options,
+}: Props<T>) => {
+  return (
+    <Autocomplete
+      multiple
+      value={values}
+      options={options}
+      onChange={(_, values) => {
+        onChange(values);
+      }}
+      renderInput={(params) => <TextField {...params} variant="standard" />}
+    />
+  );
+};
