ec20ed96c0001df5746ea57991febbe6f0b6bd68
ika-gyosen(sakai) <s.sakai4223@gmail.com>
2023-01-05 15:17:31 +0900

    テスト用のエンドポイントを追加した

--- packages/searchBFF/src/index.ts
+++ packages/searchBFF/src/index.ts
@@ -8,6 +8,9 @@ const port = process.env.PORT || 8080;
 const app = express();
 
 app.use(express.json());
+app.get('/', (req, res) => {
+  res.send('running');
+});
 
 const server = new ApolloServer({
   schema,
